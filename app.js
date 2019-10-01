a57a1a7bff824b108ee74e40b98f9c88291aed64
katerynashydlovska <shydlovskayaka@gmail.com>
2019-10-01 14:05:54 -0700

    created new files, modified JS

---
+++ app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// alert('I am here');
+// console.log('I am here');
+
+// var number = 4;
+// var string = 'string';
+// var boolean = true;
+
+// if (condition is met) {
+//     run this code
+// } else {
+//     run this code
+// }
+
+// var likesStrangeThings = promt('Hello, do you like stranger things?');
+// // likesStrangeThings = likesStrangeThings.toLower.Case();
+// // likesStrangeThings = likesStrangeThings.toUpper.Case();
+
+// if (likesStrangeThings === 'yes' || 'YES' || 'Yes'); {
+//     alert('Yes, me too!');
+// } else {
+//     alert('Go wathc it!');
+// }
+
+
+var userName = prompt('What\'s your name ?');
+// console.log('userName: ', userName);
+
+alert('Welcome ' + userName + '!');
