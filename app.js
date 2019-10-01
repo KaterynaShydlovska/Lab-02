b09c7b5e313b299ce3b2c008c3828afe126a656c
katerynashydlovska <shydlovskayaka@gmail.com>
2019-10-01 15:07:37 -0700

    finished questions

--- app.js
+++ app.js
@@ -30,31 +30,51 @@ var userName = prompt('Hey! What\'s your name ?');
 alert('Welcome ' + userName + '!');
 
 var myName = prompt('Let\'s check you! My name is Kate?');
-// console.log('myName: ', myName);
+// console.log('Yes, my name is Kate');
 
 myName = myName.toLowerCase();
-if (myName === 'yes') {
+if (myName === 'yes' || myName === 'y') {
     alert('Yeeeah');
 } else {
     alert('Ooooops, not correct!');
 }
 
 var myColor = prompt('My favorite color is orange?');
-// console.log('myColor: ', myColor);
+// console.log('No, I din't like orange color.);
 
 myColor = myColor.toLowerCase();
-if (myColor === 'no') {
+if (myColor === 'no' || myColor === 'n') {
     alert('Yeeeah, you are right!');
 } else {
     alert('Nope, I don\'t like orange color! My favorite is white.');
 }
 
 var nativeLanguage = prompt('My native language is Russian?');
-// console.log('nativeLanguage: ', nativeLanguage);
+// console.log('No, Rusian is not my native language.');
 
 nativeLanguage = nativeLanguage.toLowerCase();
-if (nativeLanguage === 'no') {
+if (nativeLanguage === 'no' || nativeLanguage === 'n') {
     alert('Yep, my native language is Ukranian');
 } else {
     alert('Nope, I speak Russian but my native is Ukranian.');
+}
+
+var pets = prompt('Do I have any pets?');
+// console.log('No, I don't have pets.');
+
+pets = pets.toLowerCase();
+if (pets === 'no' || pets === 'n') {
+    alert('Correct, unfortunately I don\'t have any.');
+} else {
+    alert('I would love to have some, but no.');
+}
+
+var likeToBeHere = prompt('Am I happy to be here?');
+// console.log('Yes, I happy to be here'.);
+
+likeToBeHere = likeToBeHere.toLowerCase();
+if (likeToBeHere === 'yes' || likeToBeHere === 'y') {
+    alert('Yaaaay, I\'m really happy to be here with all of you!!!!');
+} else {
+    alert('No, No, No. It\'s impssible!');
 }
