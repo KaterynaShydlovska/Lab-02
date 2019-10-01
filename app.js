1c8a20366a8d10b9c96ded4379a2bf89c04a67a7
katerynashydlovska <shydlovskayaka@gmail.com>
2019-10-01 16:27:40 -0700

    modified html, css. Added text and style

--- app.js
+++ app.js
@@ -1,29 +1,5 @@
 'use strict';
 
-// alert('I am here');
-// console.log('I am here');
-
-// var number = 4;
-// var string = 'string';
-// var boolean = true;
-
-// if (condition is met) {
-//     run this code
-// } else {
-//     run this code
-// }
-
-// var likesStrangeThings = promt('Hello, do you like stranger things?');
-// // likesStrangeThings = likesStrangeThings.toLowerCase();
-// // likesStrangeThings = likesStrangeThings.toUpperCase();
-
-// if (likesStrangeThings === 'yes' || 'YES' || 'Yes'); {
-//     alert('Yes, me too!');
-// } else {
-//     alert('Go wathc it!');
-// }
-
-
 var userName = prompt('Hey! What\'s your name ?');
 // console.log('userName: ', userName);
 
@@ -34,9 +10,9 @@ var myName = prompt('Let\'s check you! My name is Kate?');
 
 myName = myName.toLowerCase();
 if (myName === 'yes' || myName === 'y') {
-    alert('Yeeeah');
+  alert('Yeeeah');
 } else {
-    alert('Ooooops, not correct!');
+  alert('Ooooops, not correct!');
 }
 
 var myColor = prompt('My favorite color is orange?');
@@ -44,9 +20,9 @@ var myColor = prompt('My favorite color is orange?');
 
 myColor = myColor.toLowerCase();
 if (myColor === 'no' || myColor === 'n') {
-    alert('Yeeeah, you are right!');
+  alert('Yeeeah, you are right!');
 } else {
-    alert('Nope, I don\'t like orange color! My favorite is white.');
+  alert('Nope, I don\'t like orange color! My favorite is white.');
 }
 
 var nativeLanguage = prompt('My native language is Russian?');
@@ -54,9 +30,9 @@ var nativeLanguage = prompt('My native language is Russian?');
 
 nativeLanguage = nativeLanguage.toLowerCase();
 if (nativeLanguage === 'no' || nativeLanguage === 'n') {
-    alert('Yep, my native language is Ukranian');
+  alert('Yep, my native language is Ukranian');
 } else {
-    alert('Nope, I speak Russian but my native is Ukranian.');
+  alert('Nope, I speak Russian but my native is Ukranian.');
 }
 
 var pets = prompt('Do I have any pets?');
@@ -64,9 +40,9 @@ var pets = prompt('Do I have any pets?');
 
 pets = pets.toLowerCase();
 if (pets === 'no' || pets === 'n') {
-    alert('Correct, unfortunately I don\'t have any.');
+  alert('Correct, unfortunately I don\'t have any.');
 } else {
-    alert('I would love to have some, but no.');
+  alert('I would love to have some, but no.');
 }
 
 var likeToBeHere = prompt('Am I happy to be here?');
@@ -74,7 +50,9 @@ var likeToBeHere = prompt('Am I happy to be here?');
 
 likeToBeHere = likeToBeHere.toLowerCase();
 if (likeToBeHere === 'yes' || likeToBeHere === 'y') {
-    alert('Yaaaay, I\'m really happy to be here with all of you!!!!');
+  alert('Yaaaay, I\'m really happy to be here with all of you!!!!');
 } else {
-    alert('No, No, No. It\'s impssible!');
+  alert('No, No, No. It\'s impssible!');
 }
+
+alert('Thank you ' + userName + ' for taking small quiz about me, hope ypu had fun and you\'ll know me better!');
